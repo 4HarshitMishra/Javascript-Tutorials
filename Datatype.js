935b91e8fccdf782757fad74d4583700f625a1c3
Harshit <harshit0579.be21@chitkara.edu.in>
2022-06-05 22:47:52 +0530

    a new file added

---
+++ Datatype.js
@@ -0,0 +1,22 @@
+
+let a = true;
+
+let b = false; 
+
+let c= "String"  
+
+let d= undefined;  
+
+let e =67;
+
+console.log(typeof(a));
+
+console.log(typeof(b));
+
+console.log(typeof(c));
+
+console.log(typeof(d));
+
+console.log(typeof(e));
+
+
